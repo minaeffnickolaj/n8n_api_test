c5e3458814796362682921e75ae501d9404f7314
Nikolai Minaev <minaev.36.ru@gmail.com>
2025-07-09 23:01:34 +0300

    add OpenAI module

---
+++ src/issue_api/utils/openai.py
@@ -0,0 +1,34 @@
+import os
+import httpx
+from openai import AsyncOpenAI
+from fastapi import HTTPException
+
+from issue_api.models.issue import Issue
+
+
+class AI:
+    def __init__(self) -> None:
+        if os.getenv("OPENAI_TOKEN") is None:
+            raise EnvironmentError("Не установлена переменная токена OpenAI API")
+        else:
+            self._token = os.getenv("OPENAI_TOKEN")
+        self._client = AsyncOpenAI(api_key=self._token)
+
+
+    async def get_category(self, payload: Issue) -> str:
+        try:
+            prompt = (
+                f'Определи категорию жалобы: {payload.text}. Варианты: technical, payment, other. Ответ только одним словом.'
+            )
+            r = await self._client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "system", "content" : "Ты помощник по определению категорий жалоб"},
+                    {"role" : "user", "content" : prompt}
+                ],
+                max_tokens=1,
+                temperature=0.0 #детерминируем
+            )
+            return r.choices[0].message.content.strip().lower() #type: ignore
+        except Exception as e:
+            raise HTTPException(status_code=500, detail="Internal server error")
